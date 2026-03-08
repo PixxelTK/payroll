d8aacb461e562efe889e54a7ed1a3527023252a4
Pirapat <OUTSOURCE2365@setth.onmicrosoft.com>
2026-03-08 17:53:51 +0700

    Implement format_hours helper and update views to use it for displaying work and overtime hours

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def format_hours(hours)
+    return "-" unless hours
+
+    total_minutes = (hours * 60).round
+    h = total_minutes / 60
+    m = total_minutes % 60
+
+    "#{h}h #{m}m"
+  end
 end
