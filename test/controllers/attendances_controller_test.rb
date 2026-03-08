ed5463e2f90184054d2b4b4eb1c7c47852e55cfa
Pirapat <OUTSOURCE2365@setth.onmicrosoft.com>
2026-03-08 15:56:34 +0700

    Add attendance summary and monthly payroll summary views

--- test/controllers/attendances_controller_test.rb
+++ test/controllers/attendances_controller_test.rb
@@ -1,7 +1,32 @@
 require "test_helper"
 
 class AttendancesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @employee = employees(:one)
+    @attendance = attendances(:one)
+  end
+
+  test "should get checkin" do
+    get checkin_attendances_url(employee_id: @employee.id)
+    assert_response :success
+  end
+
+  test "should create attendance" do
+    assert_difference("Attendance.count", 1) do
+      post attendances_url, params: {
+        attendance: {
+          employee_id: @employee.id,
+          work_date: Date.tomorrow,
+          check_in: Time.current
+        }
+      }
+    end
+
+    assert_redirected_to employee_path(@employee)
+  end
+
+  test "should get checkout" do
+    get checkout_attendance_url(@attendance)
+    assert_response :success
+  end
 end
